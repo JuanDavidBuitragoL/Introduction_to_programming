51f61ef96b762bb648593067f24a91f7f130d4d6
Juan David <juan.buitragol@usantoto.edu.co>
2021-03-19 09:42:01 -0500

    Add files via upload

---
+++ Exercises_intellij_IDEA/P1T34_JAVA_HELLO_WORLD/src/USTA_TUNJA/Main.java
@@ -0,0 +1,18 @@
+package USTA_TUNJA;
+//AUTHOR: Juan D Buitrago L
+//DATE: 2021-March-16
+//DESCRIPTION: This is my first program in JAVA
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------------------------");
+        System.out.println("softtheworld version 1.0");
+        System.out.println("Maker: Juan D Buitrago");
+        System.out.println("DESCRIPTION: This software show HELLO WORLD");
+        System.out.println("--------------------------");
+
+        System.out.println("Hello world");
+    }
+}
